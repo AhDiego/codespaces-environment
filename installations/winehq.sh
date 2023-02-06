52fef431b402cf003fdc74e823f3f1a687d6f30e
Diego <116313580+AhDiego@users.noreply.github.com>
2023-02-06 20:37:19 +0200

    Update winehq.sh

--- installations/winehq.sh
+++ installations/winehq.sh
@@ -1,13 +1,11 @@
 sudo apt update
 sudo apt upgrade
-dpkg --print-architecture
-dpkg --print-foreign-architectures
 sudo dpkg --add-architecture i386
-dpkg --print-foreign-architectures
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
-sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt install wget
+wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo mv winehq-jammy.sources /etc/apt/sources.list.d/
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
+sudo apt install --install-recommends winehq-devel
 wine --version
-echo -e "{+} WineHQ Stable installed, type in terminal 'wine /path/to/application.exe' or if you have an msi 'wine msiexec /i /path/to/installer.msi'."
+echo -e "{+} WineHQ Development installed, type in terminal 'wine /path/to/application.exe' or if you have an msi 'wine msiexec /i /path/to/installer.msi'."
